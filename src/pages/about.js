b7f71ca17cd6e6bb929b0039c4e39f891aaa8eff
letpapatype <82685246+letpapatype@users.noreply.github.com>
2022-06-08 19:17:48 -0700

    end of part 2

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+// Step 1: Import React
+import * as React from 'react'
+import Layout from '../components/layout'
+
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+        <p>This will be my first JS site and project for my Youtube channel. Using the channel as the catalist for further growth and development.</p>
+    </Layout>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
---
+++ src/pages/gallery.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// calling the frame component in. <h1>This is the page tile</>
+import Frame from '../components/frame'
+
+const GalleryPage = () => {
+    return (
+        <Frame>
+            <p>This will be in as children</p>
+        </Frame>
+    )
+}
+
+export default GalleryPage 
