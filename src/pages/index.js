088ec396d8bab38b82824b683f68ae615a9c48f9
letpapatype <82685246+letpapatype@users.noreply.github.com>
2022-06-19 08:44:23 -0700

    part 3 with photos

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,18 @@
 
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image"; /* adding images */
+
 // Step 2: Define your component. The top component is the main component.
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
       <p>I'm making this by follwoing the Gatsby Tutorial.</p>
-      </Layout>
+      <StaticImage
+        alt="Jovan, in his element...circa 2015"
+        src="../images/Wbj2wa3A.png"
+      />
+    </Layout>
   );
 };
 // Step 3: Export your component
